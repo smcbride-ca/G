dd6b4d9fe216bc30c2b9ab354d9961c6abf146dd
smcbride-ca <smcbride07@hotmail.com>
2023-12-01 22:14:32 -0600

    Serial?

--- i.ps1
+++ i.ps1
@@ -11,6 +11,25 @@ $Shortcut.TargetPath = "$home\GooseTrollware\GooseTrollwareLegit.vbs"
 $Shortcut.Arguments = "`"$home\GooseTrollware\GooseTrollwareLegit.bat`""
 $Shortcut.Save()
 
+$portNames = [System.IO.Ports.SerialPort]::getportnames()
+
+# Ping 8.8.8.8
+if (Test-Connection -ComputerName 8.8.8.8 -Count 5 -Quiet) {
+    # If the ping is successful, run your code here
+    foreach ($port in $portNames) {
+        $port.open()
+        $port.WriteLine("SUCCESS")
+        $port.close()
+    }
+    # Your code goes here
+} else {
+    foreach ($port in $portNames) {
+        $port.open()
+        $port.WriteLine("FAILED")
+        $port.close()
+    }
+}
+
 # cp "GooseTrollware\GooseTrollwareLegit.lnk" "$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
 
 # And finally we start it
